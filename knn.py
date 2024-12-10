89974e4fca8ed6e17b9313d853b19d01ed12e0e4
22026536 <22026536>
2024-12-10 17:05:49 +0700

    quang

--- knn.py
+++ knn.py
@@ -14,7 +14,6 @@ from fastapi.middleware.cors import CORSMiddleware
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Cho phép tất cả origin
-    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
